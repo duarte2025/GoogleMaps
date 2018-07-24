9cce3065e7cdfde9735353b6b04bf5469e81b21b
WILLIAM <william.2025@hotmail.com>
2018-07-23 22:09:37 -0400

    first

--- app/src/main/java/br/eng/iris/googlemaps/MainActivity.kt
+++ app/src/main/java/br/eng/iris/googlemaps/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.eng.iris.googlemaps
 
+import android.location.Location
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.eng.iris.googlemaps.entity.FullParameters
@@ -13,16 +15,56 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var googleMap: GoogleMap
-    private val fullParameters: FullParameters = FullParameters("http://www.iris.eng.br/~diogo/bustracking/get.php", OperationMethod.GET )
-    private val response: HttpResponse = BaseRepository().execute(fullParameters)
-    val location: LocEntity = Gson().fromJson<LocEntity>(response.jsonResponse, object : TypeToken<LocEntity>() {}.type)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var location = LocEntity("", "", "0.0", "0.0", "")
+        class GetWeatherTask() : AsyncTask<Unit, Unit, String>() {
+
+
+            override fun doInBackground(vararg params: Unit?): String? {
+                val url = URL("http://www.iris.eng.br/~diogo/bustracking/get.php")
+                val httpClient = url.openConnection() as HttpURLConnection
+                if (httpClient.responseCode == HttpURLConnection.HTTP_OK) {
+                    try {
+                        val stream = BufferedInputStream(httpClient.inputStream)
+                        return readStream(inputStream = stream)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    } finally {
+                        httpClient.disconnect()
+                    }
+                } else {
+                    println("ERROR ${httpClient.responseCode}")
+                }
+                return null
+            }
+
+            fun readStream(inputStream: BufferedInputStream): String {
+                val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+                val stringBuilder = StringBuilder()
+                bufferedReader.forEachLine { stringBuilder.append(it) }
+                return stringBuilder.toString()
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                location = Gson().fromJson<LocEntity>(result, object : TypeToken<LocEntity>() {}.type)
+
+            }
+        }
+        GetWeatherTask().execute()
+
         setContentView(R.layout.activity_main)
         mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
